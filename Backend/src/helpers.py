311607c78bcff1b2e8e9679c047a8e11f7fc3a5f
Xiuyu Li <1183135553@qq.com>
2019-03-02 17:55:38 -0500

    Implement initial fuel function

--- Backend/src/helpers.py
+++ Backend/src/helpers.py
@@ -12,5 +12,24 @@ def gcloud_to_food(post_body):
             return food
 
 
+def calc_emission_driver(mdf, division, carline, g_to_m, d):
+    return mdf[division][carline][g_to_m] / d * 9.072 / 1000
+
+
+def calc_emission(persona):
+    if persona == "driver":
+        return 120
+    if persona == "Farmers/Producers":
+        return 80
+    if persona == "Storage companies":
+        return 50
+    if persona == "Food processor":
+        return 50
+    if persona == "Trader/Distributor":
+        return 60
+
+
+
+
 
 
