8d611a5196fc4121c52a56c40217f71b17b779cf
zhen <luzhen2000@outlook.com>
2019-03-02 16:32:34 -0500

    add db and route

---
+++ Backend/src/db.py
@@ -0,0 +1,46 @@
+from flask_sqlalchemy import SQLAlchemy
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+
+    # User information
+    username = db.Column(db.String, nullable=False)
+    persona = db.Column(db.String, nullable = False)
+    emission = db.Column(db.String, nullable = False)
+    chain_id = db.Column(db.Integer, db.ForeignKey(supplychain.id),nullable = False)
+
+    def __init__(self, **kwargs):
+        self.username = kwargs.get('username')
+        self.persona = kwargs.get('persona')
+        self.emission = kwargs.get('emission')
+        self.chain_id = kwargs.get('chain_id')
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'persona': self.persona,
+            'chain_id': self.chain_id
+        }
+
+
+class Supplychain(db.Model):
+    __tablename__ = 'supplychain'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, default=0)
+    total_emission = db.Column(db.Integer, nullable=False)
+    stops = db.relationship('Stop')
+    
+
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', '')
+
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'total_emission': self.total_emission
+        }
