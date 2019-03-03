9618dbde7e40209f8dd88b921dd57d0948e59021
Xiuyu Li <1183135553@qq.com>
2019-03-03 01:15:36 -0500

    Update helper function

--- Backend/src/helpers.py
+++ Backend/src/helpers.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 df = pd.read_excel('./data/car_efficiency.xlsx')
 df.sort_values(by=['Mfr Name'])
+food_df = pd.read_csv('./data/Carbon_Emissions_per_Serving_and_per_Calorie.csv')
 
 
 def calc_emission_driver(mdf, division, carline, g_to_m, index, eng, cyl, tran, d):
@@ -10,6 +11,11 @@ def calc_emission_driver(mdf, division, carline, g_to_m, index, eng, cyl, tran, 
     (df['Index (Model Type Index)'] == index) & (df['Eng Displ'] == eng) & (df['# Cyl'] == cyl) & 
     (df['Transmission'] == tran)]
     return vehicle['Comb Unadj FE - Conventional Fuel'].iloc[0] / d * 9.072 / 1000
+
+
+def calc_emission_food_base(type):
+    food = food_df.loc[(food_df['Food'] == type)]
+    return food['Grams CO2e per Serving'].iloc[0]
 
 
 def calc_emission(persona):
