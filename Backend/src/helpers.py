af6e64560ddc8ec5068341b7c62c77fe0de587e9
Xiuyu Li <1183135553@qq.com>
2019-03-02 21:41:30 -0500

    Update calc_emission_driver

--- Backend/src/helpers.py
+++ Backend/src/helpers.py
@@ -1,8 +1,15 @@
-# import pandas as pd
+import pandas as pd
+
+df = pd.read_excel('./data/car_efficiency.xlsx')
+df.sort_values(by=['Mfr Name'])
 
 
-def calc_emission_driver(mdf, division, carline, g_to_m, d):
-    return mdf[division][carline][g_to_m] / d * 9.072 / 1000
+def calc_emission_driver(mdf, division, carline, g_to_m, index, eng, cyl, tran, d):
+    vehicle = df.loc[(df['Mfr Name'] == mdf) & (df['Division'] == division) & 
+    (df['Carline'] == carline) & (df['Verify Mfr Cd'] == g_to_m) & 
+    (df['Index (Model Type Index)'] == index) & (df['Eng Displ'] == eng) & (df['# Cyl'] == cyl) & 
+    (df['Transmission'] == tran)]
+    return vehicle['Comb Unadj FE - Conventional Fuel'].iloc[0] / d * 9.072 / 1000
 
 
 def calc_emission(persona):
